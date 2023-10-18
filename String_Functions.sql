ae187ee4ae136c58007c12409b556f6aa52a4658
Arjun Suthaharan <arjunsutha@gmail.com>
2023-10-18 13:49:52 -0400

    More string functions added

--- String_Functions.sql
+++ String_Functions.sql
@@ -31,3 +31,34 @@ FROM tblPerson
 Group by SUBSTRING(email, 
 CHARINDEX('@', email) + 1, 
 LEN(email) - CHARINDEX('@', email))
+
+-- REPLICATE function with SUBSTRING and CHARINDEX to mask email string
+
+SELECT name, SUBSTRING(email, 1, 2) + REPLICATE('*', 5) + 
+SUBSTRING(email, CHARINDEX('@', email), LEN(email) - CHARINDEX('@', email)+1) as email
+FROM tblPerson
+
+-- SPACE function to add space in string between names
+
+SELECT firstName + Space(5) + lastName as fullName
+FROM tblPerson
+
+
+-- PATINDEX function to return the starting position of first occurance of pattern
+
+SELECT email, PATINDEX('%@d.com', email) AS firstOccurance
+FROM tblPerson
+WHERE PATINDEX('%@d.com', email) > 0
+
+-- REPLACE function to replace specified string value with another string value
+
+SELECT email, REPLACE(email, '.com', '.net') as convertedEmail
+FROM tblPerson
+
+SELECT * from tblPerson
+
+-- STUFF Function to inject string at specified point, and removes characters specified
+
+SELECT email, STUFF(email, 2,2,'******') as stuffedEmail
+FROM tblPerson
+
