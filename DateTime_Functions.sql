5b2e1a8487a6b906b6ead374a7df4a61f0866071
Arjun Suthaharan <arjunsutha@gmail.com>
2023-10-23 19:45:43 -0400

    Update DateTime_Functions.sql

--- DateTime_Functions.sql
+++ DateTime_Functions.sql
@@ -12,9 +12,9 @@ cDateTimeOffset DATETIMEOFFSET(7),
 
 SELECT * FROM tblDateTime
 
--- GETDATE function used to retireve current system date and time
+-- GETDATE function used to retrieve current system date and time
 
-SELECT GETDATE()
+SELECT GETDATE() -- returns current date on system
 
 -- Filling tblDateTime table with datetime data using GETDATE function
 
@@ -39,3 +39,30 @@ SELECT GETUTCDATE
 -- SYSUTCDATETIME function
 
 SELECT SYSUTCDATETIME
+
+-- ISDATE function to retrieve if given value is valid date, time or datetime (datetime2 data types return 0)
+
+SELECT ISDATE('NOTVALID')  -- will return 0
+
+SELECT ISDATE(GETDATE()) -- will return 1
+
+SELECT ISDATE('2000-01-03') -- will return 1
+
+-- DAY function returns day number of month in given date
+
+SELECT DAY('01/25/2000') -- returns 25
+
+-- MONTH function returns month number in given date
+
+SELECT MONTH('01/25/2000') -- returns 1
+
+-- YEAR function returns year number in given date
+
+SELECT YEAR('01/25/2000') -- returns 2000
+
+
+-- DATENAME function returns a string that represents specified input
+
+SELECT DATENAME(Day,'2000-01-25') -- returns 25
+SELECT DATENAME(WEEKDAY, '2000-01-25') -- returns Tuesday
+SELECT DATENAME(Month,'2000-05-25') -- returns May
